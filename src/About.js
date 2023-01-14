db2b69ae087de9d353bf2ea8f1e965709e12500f
peleg vadbeker <peleg40@gmail.com>
2023-01-14 13:44:40 +0200

    update home.js , about.js , and made links between home,about,report

--- src/About.js
+++ src/About.js
@@ -1,7 +1,23 @@
-function About() {
+import React from 'react';
+import './About.css'
+const About = () => {
     return (
-        <div>
-            <h1>This is About!!!</h1>
+        <div className="aboutclass">
+            <h1>About Our App</h1>
+            <p>Our app is a platform that connects users with local events and activities. It allows users to search for events based on location, date, and category, and also allows them to RSVP to events and receive updates on any changes. </p>
+            <h2>Meet Our Team</h2>
+            <h3>Team Member 1</h3>
+            <p>Name: Peleg Vadbeker</p>
+            <p>Role: Computer Science student - HIT </p>
+            <p>Bio: John is a experienced software developer with a background in event management. He is passionate about using technology to connect people with their communities and make it easier to discover new experiences.</p>
+            <h3>Team Member 2</h3>
+            <p>Name: Eden Blau</p>
+            <p>Role: Computer Science student - HIT </p>
+            <p>Bio: Jane is a talented designer with a focus on creating user-friendly interfaces. She has a keen eye for detail and is dedicated to making sure our app is easy and enjoyable for users to use.</p>
+            <h3>Team Member 3</h3>
+            <p>Name: Dudi Kreis</p>
+            <p>Role: Computer Science student - HIT </p>
+            <p>Bio: Bob is a skilled developer with expertise in building scalable and robust back-end systems. He is dedicated to making sure our app is fast and reliable for users.</p>
         </div>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import './Home.css';
+import { useState ,useEffect} from 'react';
+
 
 const Home = () => {
+
+
+    useEffect(() => {
+        const inputsFromLocalStorage = JSON.parse(localStorage.getItem('inputs'));
+        if (inputsFromLocalStorage) {
+            setInputs(inputsFromLocalStorage);
+        }
+    }, []);
+
+    const [date, setDate] = useState('');
+    const [item, setItem] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('food');
+    const [description, setDescription] = useState('');
+    const [inputs, setInputs] = useState([]);
+
+
 
     return (
         <div className="Home">
@@ -11,20 +30,20 @@ const Home = () => {
 
                 <div className="inputGroup">
                     <label htmlFor='name'>Date</label>
-                    <input type="date" required="" autoComplete={'off'}/>
+                    <input type="date" value={date} onChange={e=>setDate(e.target.value)} required="" autoComplete={'off'}/>
                 </div>
 
                 <div className="inputGroup">
-                    <label>Item: </label><input type="text"/>
+                    <label>Item: </label><input type="text" value={item} onChange={e=>setItem(e.target.value)}/>
                 </div>
 
                 <div className="inputGroup">
-                    <label>Price: </label><input type="text"/>
+                    <label>Price: </label><input type="text" value={price} onChange={e=>setPrice(e.target.value)}/>
                 </div>
 
                 <div className="inputGroup">
                     <label>Category:</label>
-                    <select id>
+                    <select value={category} onChange={e=>setCategory(e.target.value)}>
                         <option value="food">Food</option>
                         <option value="personalSpending">Personal spending</option>
                         <option value="children">Children</option>
@@ -39,15 +58,32 @@ const Home = () => {
                 </div>
 
                 <div className="inputGroup">
-                    <label>Description: </label><textarea placeholder="Enter description"></textarea>
+                    <label>Description: </label><textarea value={description} onChange={e=>setDescription(e.target.value)} placeholder="Enter description"></textarea>
                 </div>
 
                 <div className="submit">
-                    <button id="plus">Add cost</button>
+                    <button id="plus" onClick={()=>{
+                        setInputs([...inputs,{date,item,price,category,description}]);
+                        localStorage.setItem('inputs', JSON.stringify([...inputs,{date,item,price,category,description}]));
+                    }}>Add cost</button>
                 </div>
+
             </div>
 
+            {/*The component then maps over the items in the inputs state variable and displays them in a list*/}
+            {inputs.map((input, index) => (
+                <li className={'list-item'} key={index}>
+                    Date: {input.date} Item: {input.item} Price: {input.price} Category: {input.category} Description: {input.description}
+                    <button onClick={()=>{
+                        inputs.splice(index, 1);
+                        setInputs([...inputs]);
+                        localStorage.setItem('inputs', JSON.stringify(inputs));
+                    }}>Delete</button>
+                </li>
+            ))}
+
         </div>
+
     );
 }
 export default Home;
